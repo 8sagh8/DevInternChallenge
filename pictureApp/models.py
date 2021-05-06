98c6c7283c2c0b069e9b3c2b5cfe0ae79865a3dc
SAGH <42285767+8sagh8@users.noreply.github.com>
2021-05-06 15:51:34 -0400

    feat: Picture private or public status

--- pictureApp/models.py
+++ pictureApp/models.py
@@ -5,6 +5,7 @@ from django.db import models
 class Photo(models.Model):
     title = models.CharField(max_length=50, default="Not Available")
     image = models.ImageField()
+    status = models.BooleanField(default=True)
     
     def __str__(self):
-        return self.title
+        return self.title + ' status: ' + str(self.status)
