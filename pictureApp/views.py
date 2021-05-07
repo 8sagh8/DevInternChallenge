1ab45345f0599891c08f4663d6d0565b3cff150d
SAGH <42285767+8sagh8@users.noreply.github.com>
2021-05-07 04:39:29 -0400

    feat: secure images and protection

--- pictureApp/views.py
+++ pictureApp/views.py
@@ -5,11 +5,30 @@ import os
 
 from .form import *
 
+#Function to get Current User's Photo and others Public Only
+def currentPhoto(request):
+    photoList = []
+    currentUser = (request.user)
+    photo=Photo.objects.all()
+    
+    for p in photo:
+        temp = []
+        if str(currentUser) == p.user:
+            temp.append(True)
+            temp.append(p)
+            photoList.append(temp)
+        elif p.status == False:
+            temp.append(False)
+            temp.append(p)
+            photoList.append(temp)
+    photoList.reverse()
+
+    return photoList
+
 # Create your views here.
 def index(request):
     auth_person = request.user
-    print("auth=>", auth_person, " type: ", type(auth_person), " str:", str(auth_person), flush=True)
-    photoList = []
+    
     if request.method == "POST":
         form=ImageForm(data=request.POST,files=request.FILES)
         if form.is_valid():
@@ -20,12 +39,8 @@ def index(request):
     else:
         form=ImageForm() 
     
-    
-    photo=Photo.objects.all()
-    for p in photo:
-        photoList.append(p)
+    photoList = currentPhoto(request)
 
-    photoList.reverse()
     return render(request,"pictureApp/index.html", {
         "photo":photoList, 
         "form": form,
@@ -48,7 +63,6 @@ def modifyPhoto(request, _id):
 # Persisting Photo title after modification
 def persistPhoto(request, _id):
     form=ImageForm()
-    photoList = []
 
     if request.method == "POST":
         title = request.POST['newTitle']
@@ -56,10 +70,7 @@ def persistPhoto(request, _id):
         photo_obj.title= title
         photo_obj.save()
 
-    obj = Photo.objects.all()
-    for p in obj:
-        photoList.append(p) 
-    photoList.reverse()
+    photoList = currentPhoto(request)
 
     return redirect ("/", {
         "photo": photoList,
@@ -69,17 +80,13 @@ def persistPhoto(request, _id):
 
 
 def deletePhoto(request, _id):
-    photo_obj = Photo.objects.get(id=eval(_id))
-    print("image name:", photo_obj.image, flush=True)
-    photo_obj.delete()
+    photo_obj = Photo.objects.get(id=eval(_id)) # get photo object by id
+    file_path = 'static/photosFolder/' + str(photo_obj.image)   # get file full path
+    os.remove(file_path)    # delete file from local folder
+    photo_obj.delete()      # delete object data from database
 
     form=ImageForm()
-    photoList = []
-
-    obj = Photo.objects.all()
-    for p in obj:
-        photoList.append(p) 
-    photoList.reverse()
+    photoList = currentPhoto(request)
     
     return redirect("/", {
         "photo": photoList,
@@ -87,21 +94,18 @@ def deletePhoto(request, _id):
     }) 
 
 def deleteMultiPhoto(request):
- 
+    print("~~~~~ I am In deleteMulti", flush=True )
     if request.method == 'POST':
         id_List = request.POST.getlist('data[]')
 
         for _id in id_List:
             p_obj = Photo.objects.get(id=eval(_id))
+            file_path = 'static/photosFolder/' + str(photo_obj.image)   # get file full path
+            os.remove(file_path)    # delete file from local folder
             p_obj.delete()
 
     form=ImageForm()
-    photoList = []
-
-    obj = Photo.objects.all()
-    for p in obj:
-        photoList.append(p) 
-    photoList.reverse()
+    photoList = currentPhoto(request)
 
     return redirect('/', {
         "photo": photoList,
@@ -119,8 +123,9 @@ def searchPhoto(request):
 
     if search_value != None:
         for p in obj:
-            if search_value in p.title:
-                photoList.append(p)
+            if p.status == False or (p.user == str(request.user)): 
+                if search_value in p.title.lower():
+                    photoList.append(p)
     
     photoList.reverse()
 
@@ -138,12 +143,7 @@ def statusChange(request, _id):
     photo_obj.save()
 
     form=ImageForm()
-    photoList = []
-
-    obj = Photo.objects.all()
-    for p in obj:
-        photoList.append(p) 
-    photoList.reverse()
+    photoList = currentPhoto(request)
 
     return redirect('/', {
         "photo": photoList,
